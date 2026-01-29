c10156335408af7d22e65a15992c4f2b590ef0af
E Bharati Patra <bharati.patra@bridgelabz.com>
2026-01-29 15:41:47 +0530

    [Bharati] Added:UC2-Render OOPS Banner using print statement

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * This class prints the word "OOPS" in a banner format
+ * using asterisks (*) and spaces.
+ *
+ * Each line is printed using individual print statements.
+ * No loops are used.
+ *
+ * @author Bharati
+ * @version 2
+ */
+public class Main {
 
-public class Main{
-  public static void main(String args[]) {
-    System.out.println("Welcome to Java-RFP");
-  }
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(
+                "   ***   " + "   ***   " + "  *****  " + "  *****  "
+        );
+
+        // Line 2
+        System.out.println(
+                " **   ** " + " **   ** " + " **   ** " + " **      "
+        );
+
+        // Line 3
+        System.out.println(
+                " **   ** " + " **   ** " + " **   ** " + " **      "
+        );
+
+        // Line 4
+        System.out.println(
+                " **   ** " + " **   ** " + "  *****  " + "  *****  "
+        );
+
+        // Line 5
+        System.out.println(
+                " **   ** " + " **   ** " + " **      " + "      ** "
+        );
+
+        // Line 6
+        System.out.println(
+                " **   ** " + " **   ** " + " **      " + "      ** "
+        );
+
+        // Line 7
+        System.out.println(
+                "   ***   " + "   ***   " + " **      " + "  *****  "
+        );
+    }
 }
