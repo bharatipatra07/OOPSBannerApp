99c12118951c965f5336c8c7fe43eb0886665cf8
E Bharati Patra <bharati.patra@bridgelabz.com>
2026-01-29 15:39:38 +0530

    [Bharati] Added:UC1-Print OOPS to Console

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application that displays the Object
+ * Oriented Programming System OOPS acronym to the console.
+ *
+ * @author Bharati
+ * @version 1.0
+ */
 
-public class Main{
-  public static void main(String args[]) {
-    System.out.println("Welcome to Java-RFP");
-  }
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
 }
