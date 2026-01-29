59520670eb060a8eb4ab99242de2639fad8704a4
E Bharati Patra <bharati.patra@bridgelabz.com>
2026-01-29 16:40:19 +0530

    [Bharati] Added:UC8 - Implement banner rendering using HashMap for character patterns

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,79 @@
 
-public class Main{
-  public static void main(String args[]) {
-    System.out.println("Welcome to Java-RFP");
-  }
+import java.util.HashMap;
+/**
+ * OOPSBannerApp UC8 – Advanced OOP Concepts Implementation
+ *
+ * This use case uses the Java Collections Framework (HashMap)
+ * to manage ASCII art patterns for characters and render
+ * the banner dynamically for a given message.
+
+ *
+ * @author Bharati
+ * @version 8.0
+ */
+public class Main {
+
+    public static HashMap<Character, String[]> createCharacterMap() {
+
+        HashMap<Character, String[]> charMap = new HashMap<>();
+
+        charMap.put('O', new String[]{
+                "   ***   ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                "   ***   "
+        });
+
+        charMap.put('P', new String[]{
+                "  *****  ",
+                " **   ** ",
+                " **   ** ",
+                "  *****  ",
+                " **      ",
+                " **      ",
+                " **      "
+        });
+
+        charMap.put('S', new String[]{
+                "  *****  ",
+                " **      ",
+                " **      ",
+                "  *****  ",
+                "      ** ",
+                "      ** ",
+                "  *****  "
+        });
+
+        return charMap;
+    }
+
+    public static void displayBanner(String message,
+                                     HashMap<Character, String[]> charMap) {
+
+
+        int patternHeight = charMap.get(message.charAt(0)).length;
+
+        for (int line = 0; line < patternHeight; line++) {
+            StringBuilder sb = new StringBuilder();
+
+
+        for (char ch : message.toCharArray()) {
+                String[] pattern = charMap.get(ch);
+                sb.append(pattern[line]).append(" ");
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HashMap<Character, String[]> charMap = createCharacterMap();
+        String message = "OOPS";
+
+        displayBanner(message, charMap);
+    }
 }
