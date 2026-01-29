db092f30ff20af3ba971f178797ea15afd5702e3
E Bharati Patra <bharati.patra@bridgelabz.com>
2026-01-29 15:46:40 +0530

    [Bharati] Added:UC3: Render OOPS banner using String.join() for better efficiency

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+/**
+ * OOPSBannerApp UC3 – OOPS Banner Application (Use Case 3)
+ *
+ * This class improves UC2 by using String.join() instead of the +
+ * operator to build each line of the "OOPS" banner.
+ *
+ * Using String.join() reduces intermediate String object creation,
+ * improves memory efficiency, and enhances readability.
+ *
+ * @author Bharati
+ * @version 3.0
+ */
 
-public class Main{
-  public static void main(String args[]) {
-    System.out.println("Welcome to Java-RFP");
-  }
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Hint: Use String.join(" ", ...) to join the parts of each line with spaces.
+
+        System.out.println(String.join(" ",
+                "  ***  ", "  ***  ", " ***** ", " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                " **   ** ", " **   ** ", " **    ", " **    "
+        ));
+
+        System.out.println(String.join(" ",
+                " **   ** ", " **   ** ", " **    ", " **    "
+        ));
+
+        System.out.println(String.join(" ",
+                " **   ** ", " **   ** ", " ***** ", " ***** "
+        ));
+
+        System.out.println(String.join(" ",
+                " **   ** ", " **   ** ", " **    ", "    ** "
+        ));
+
+        System.out.println(String.join(" ",
+                " **   ** ", " **   ** ", " **    ", "    ** "
+        ));
+
+        System.out.println(String.join(" ",
+                "  ***  ", "  ***  ", " **    ", " ***** "
+        ));
+    }
 }
