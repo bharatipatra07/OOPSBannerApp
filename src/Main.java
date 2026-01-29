e9614c1cb7954ff7ebd43eb68c680ed159e2019d
E Bharati Patra <bharati.patra@bridgelabz.com>
2026-01-29 16:06:22 +0530

    [Bharati] Added:UC5 - Render OOPS banner using inline array initialization

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+/**
+ * OOPSBannerApp UC5 – Render OOPS as Banner using Inline Array Initialization
+ *
+ * This use case extends UC4 by defining and populating the String array
+ * at the time of declaration using String.join() to create each line
+ * of the banner. This improves code readability and maintainability.
+ *
+ * @author Bharati
+ * @version 5.0
+ */
+public class Main {
 
-public class Main{
-  public static void main(String args[]) {
-    System.out.println("Welcome to Java-RFP");
-  }
+    public static void main(String[] args) {
+
+        String[] lines = {
+                String.join(" ",
+                        "   ***   ", "   ***   ", "  *****  ", "  *****  "),
+                String.join(" ",
+                        " **   ** ", " **   ** ", " **   ** ", " **      "),
+                String.join(" ",
+                        " **   ** ", " **   ** ", " **   ** ", " **      "),
+                String.join(" ",
+                        " **   ** ", " **   ** ", "  *****  ", "  *****  "),
+                String.join(" ",
+                        " **   ** ", " **   ** ", " **      ", "      ** "),
+                String.join(" ",
+                        " **   ** ", " **   ** ", " **      ", "      ** "),
+                String.join(" ",
+                        "   ***   ", "   ***   ", " **      ", "  *****  ")
+        };
+
+     for (String line : lines) {
+            System.out.println(line);
+        }
+    }
 }
